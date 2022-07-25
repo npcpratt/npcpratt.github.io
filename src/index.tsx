7b04d66612a99c7cb21552cabeec41129eb34f92
Pratik <npc.prat@gmail.com>
2022-07-25 12:51:49 +0530

    Minor fix

--- src/index.tsx
+++ src/index.tsx
@@ -11,7 +11,7 @@ const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 
-export default function ScrollToTop() {
+function ScrollToTop() {
   const { pathname } = useLocation();
 
   useEffect(() => {
